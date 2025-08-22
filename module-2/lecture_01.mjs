37a111b5b90fe9d720b11d6c6cb9a424783d7796
vilhelm <vilhelmlangemyr@live.com>
2025-08-22 13:46:55 +0200

    Initials commit with course files

--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -1,7 +1,4 @@
 "use strict";
 import { printOut } from "../../common/script/utils.mjs";
 
-//create a function that print hello world to console
-function helloWorld() {
-  printOut("Hello World");
-}
+
