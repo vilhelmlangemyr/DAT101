71fc6054c4502bc7965077c7862d251ca021ddb0
vilhelm <vilhelmlangemyr@live.com>
2025-08-22 13:45:21 +0200

    test

--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -1,7 +1,7 @@
+"use strict";
+import { printOut } from "../../common/script/utils.mjs";
 
-"use strict"
-import {printOut} from "../../common/script/utils.mjs";
-
-
-
-
+//create a function that print hello world to console
+function helloWorld() {
+  printOut("Hello World");
+}
