2ce5ebf534deab4be0c663fe4f709d5e7ac0b064
vilhelm <vilhelmlangemyr@live.com>
2026-01-05 12:29:05 +0100

    Ferdig med alle

--- module-4/task_4-1/task_4-1.mjs
+++ module-4/task_4-1/task_4-1.mjs
@@ -3,35 +3,213 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Oppgave 1");
+
+const Accounttype = {
+    Normal: "Brukskonto",
+    Savings: "Sparekonto",
+    Credit: "Kredittkonto",
+    Pension: "Pensjonskonto"
+};
+const CurrencyTypes = {
+  NOK: { value: 1.0000, name: "Norske kroner", denomination: "kr" },
+  EUR: { value: 0.0985, name: "Europeiske euro", denomination: "€" },
+  USD: { value: 0.1091, name: "United States dollar", denomination: "$" },
+  GBP: { value: 0.0847, name: "Pound sterling", denomination: "£" },
+  INR: { value: 7.8309, name: "Indiske rupee", denomination: "₹" },
+  AUD: { value: 0.1581, name: "Australske dollar", denomination: "A$" },
+  PHP: { value: 6.5189, name: "Filippinske peso", denomination: "₱" },
+  SEK: { value: 1.0580, name: "Svenske kroner", denomination: "kr" },
+  CAD: { value: 0.1435, name: "Canadiske dollar", denomination: "C$" },
+  THB: { value: 3.3289, name: "Thai baht", denomination: "฿" }
+};
+printOut(Object.values(Accounttype).join(", "));
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Oppgave 2");
+
+class TAccount {
+    #type;
+    #balance = 0;
+    #withdrawCount = 0;
+    #currencyType = "NOK";
+
+    constructor(aType) {
+        this.#type = aType;
+    }
+
+    toString() {
+        return this.#type;
+    }
+
+    setType(aType) {
+        printOut(`Account is changed from ${this.#type} to ${aType}`);
+        this.#type = aType;
+        this.#withdrawCount = 0;
+    }
+
+    getBalance() {
+        return this.#balance;
+    }
+
+    // ---------- Part 7: deposit kan ta beløp i annen valuta ----------
+    deposit(aAmount, aType = this.#currencyType) {
+        // konverter beløpet til kontovaluta
+        const amountInAccountCurrency = this.#convertAmount(aAmount, aType, this.#currencyType);
+
+        this.#balance += amountInAccountCurrency;
+        this.#withdrawCount = 0;
+
+        const accountSymbol = CurrencyTypes[this.#currencyType].denomination;
+        const inputSymbol = CurrencyTypes[aType].denomination;
+
+        printOut(
+            `Deposit of ${aAmount}${inputSymbol} (${amountInAccountCurrency.toFixed(2)}${accountSymbol}), new balance is ${this.#balance.toFixed(2)}${accountSymbol}`
+        );
+    }
+
+    // ---------- Part 7: withdraw kan ta beløp i annen valuta ----------
+    withdraw(aAmount, aType = this.#currencyType) {
+        switch (this.#type) {
+            case Accounttype.Pension:
+                printOut("You can't withdraw from a Pensjonskonto!");
+                return;
+            case Accounttype.Savings:
+                if (this.#withdrawCount >= 3) {
+                    printOut("You can't Withdraw from a Sparekonto more than three times!");
+                    return;
+                }
+                break;
+        }
+
+        // konverter beløpet til kontovaluta
+        const amountInAccountCurrency = this.#convertAmount(aAmount, aType, this.#currencyType);
+
+        if (this.#balance < amountInAccountCurrency) {
+            printOut("Withdrawal failed: insufficient funds");
+            return;
+        }
+
+        this.#balance -= amountInAccountCurrency;
+        this.#withdrawCount++;
+
+        const accountSymbol = CurrencyTypes[this.#currencyType].denomination;
+        const inputSymbol = CurrencyTypes[aType].denomination;
+
+        printOut(
+            `Withdrawal of ${aAmount}${inputSymbol} (${amountInAccountCurrency.toFixed(2)}${accountSymbol}), new balance is ${this.#balance.toFixed(2)}${accountSymbol}`
+        );
+    }
+
+    setCurrencyType(aType) {
+        if (this.#currencyType === aType) return;
+        const oldCurrency = this.#currencyType;
+
+        this.#balance = this.#currencyConvert(aType);
+        this.#currencyType = aType;
+        printOut(
+            `The account currency has change from ${CurrencyTypes[oldCurrency].name} to ${CurrencyTypes[aType].name}`
+        );
+        const symbol = CurrencyTypes[this.#currencyType].denomination;
+        printOut(`New balance is ${this.#balance.toFixed(2)}${symbol}`);
+    }
+
+    #currencyConvert(aType) {
+        // Sjekk at valutaene finnes
+        const oldCurrency = CurrencyTypes[this.#currencyType];
+        const newCurrency = CurrencyTypes[aType];
+        if (!oldCurrency || !newCurrency) return this.#balance;
+
+        const newAmount = this.#balance * (newCurrency.value / oldCurrency.value);
+        return newAmount;
+    }
+
+    #convertAmount(amount, fromType, toType) {
+        const from = CurrencyTypes[fromType];
+        const to = CurrencyTypes[toType];
+        if (!from || !to) return amount; // fallback hvis ukjent valuta
+
+        // Riktig formel: amount * (to.value / from.value)
+        const converted = amount * (to.value / from.value);
+        return converted;
+    }
+
+} 
+
+const myAccount = new TAccount(Accounttype.Normal);
+
+printOut("myAccount = " + myAccount.toString());
+
+myAccount.setType(Accounttype.Savings);
+
+printOut("myAccount = " + myAccount.toString());
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Oppgave 3");
+
+myAccount.deposit(100);
+myAccount.withdraw(25);
+printOut("My account balance is " + myAccount.getBalance());
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Oppgave 4");
+
+const need = 100 - myAccount.getBalance();
+if (need > 0) {
+    myAccount.deposit(need);
+}
+
+myAccount.withdraw(30);
+myAccount.withdraw(30);
+myAccount.withdraw(30);
+myAccount.withdraw(30); // dette skal stoppes pga 3 uttak
+
+// Bytt til Pension (skal ikke gå)
+myAccount.setType(Accounttype.Pension);
+myAccount.withdraw(10); // skal ikke gå
+
+myAccount.setType(Accounttype.Savings);
+myAccount.withdraw(10); // dette skal gå
+
+printOut(newLine);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Oppgave 5");
+myAccount.deposit(150);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Oppgave 6");
+
+// Bytt til SEK
+myAccount.setCurrencyType("SEK");
+
+// Bytt til USD
+myAccount.setCurrencyType("USD");
+
+// Bytt tilbake til NOK
+myAccount.setCurrencyType("NOK");
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+printOut("oppgave 7");
+
+if (myAccount.getBalance() < 150) {
+    const difference = 150 - myAccount.getBalance();
+    myAccount.deposit(difference);
+}
+
+myAccount.deposit(12, "USD");
+myAccount.withdraw(10, "GBP");
+
+myAccount.setCurrencyType("CAD");   // Norske kr → Canadiske dollar
+myAccount.setCurrencyType("INR");   // Canad
--- module-4/task_4-2/task_4-2.mjs
+++ module-4/task_4-2/task_4-2.mjs
@@ -71,31 +71,148 @@ printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const boys = ["Jakob", "Lucas", "Emil", "Oskar", "Oliver", "William", "Filip", "Noah", "Elias", "Isak", "Henrik", "Aksel", "Kasper", "Mathias", "Jonas", "Tobias", "Liam", "Håkon", "Theodor", "Magnus"];
+const allNames = girls.concat(boys);
+printOut(allNames);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+class TBook {
+  #title;
+  #author;
+  #isbn;
+  constructor(aTitle, aAuthor, aISBN){
+    this.#title = aTitle;
+    this.#author = aAuthor;
+    this.#isbn = aISBN;
+  }
+  toString(){
+    return `Title: ${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`;
+  }
+}
+
+const book1 = new TBook("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
+const book2 = new TBook("To Kill a Mockingbird", "Harper Lee", "9780061120084");
+const book3 = new TBook("1984", "George Orwell", "9780451524935");
+const bookArray = [book1, book2, book3];
+let part6Text = "";
+for (const book of bookArray) {
+  part6Text += book.toString() + "<br>";
+}
+printOut(part6Text);
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const EWeekDays = {
+    WeekDay1: { value: 0x01, name: "Mandag" },
+    WeekDay2: { value: 0x02, name: "Tirsdag" },
+    WeekDay3: { value: 0x04, name: "Onsdag" },
+    WeekDay4: { value: 0x08, name: "Torsdag" },
+    WeekDay5: { value: 0x10, name: "Fredag" },
+    WeekDay6: { value: 0x20, name: "Lørdag" },
+    WeekDay7: { value: 0x40, name: "Søndag" },
+
+    Workdays: {
+        value: 0x01 + 0x02 + 0x04 + 0x08 + 0x10,
+        name: "Arbeidsdager"
+    },
+
+    Weekends: {
+        value: 0x20 + 0x40,
+        name: "Helg"
+    },
+};
+
+const weekDayKeys = Object.keys(EWeekDays);
+let part7Text = "";
+for (const key of weekDayKeys) {
+    const day = EWeekDays[key];
+    part7Text += `${key}: Value = ${day.value}, Name = ${day.name}` + newLine;
+}
+printOut(part7Text);
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const randomNumbers = [];
+for (let i = 0; i < 35; i++) {
+    const randomNum = Math.floor(Math.random() * 20) + 1;
+    randomNumbers.push(randomNum);
+}
+function ascending(a, b) {
+    return a - b;
+}
+function descending(a, b) {
+    return b - a;
+}
+const ascendingNumbers = [...randomNumbers].sort(ascending);
+const descendingNumbers = [...randomNumbers].sort(descending);
+printOut("Original Array: " + randomNumbers.join(", "));
+printOut("Ascending Order: " + ascendingNumbers.join(", "));
+printOut("Descending Order: " + descendingNumbers.join(", "));
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const frequency = {};
+for(let i = 0; i < randomNumbers.length; i++){
+  if(frequency[randomNumbers[i]]){
+    frequency[randomNumbers[i]] += 1;
+  }else{
+    frequency[randomNumbers[i]] = 1;
+  }
+}
+let part9Text1 = "Number Frequencies:" + newLine;
+for(const number in frequency){
+  part9Text1 += `Number ${number} occurs ${frequency[number]} times.` + newLine;
+}
+printOut(part9Text1);
+printOut(newLine);
+
+function frequencySort(a, b){
+  if(frequency[b] === frequency[a]){
+    return a - b; // Ascending order for numbers
+  }
+  return frequency[b] - frequency[a]; // Descending order for frequency
+}
+
+const frequencyKeys = Object.keys(frequency);
+frequencyKeys.sort(frequencySort);
+const frequencyValues = [];
+for(let i = 0; i < frequencyKeys.length; i++){
+  const value = parseInt(frequencyKeys[i]);
+  frequencyValues.push(value);
+}
+let part9Text2 = "Frequencies Sorted:" + newLine;
+for(let i = 0; i < frequencyValues.length; i++){
+  const num = frequencyValues[i];
+  part9Text2 += `Number ${num} occurs ${frequency[num]} times.` + newLine;
+}
+printOut(part9Text2);
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const rows = 5;
+const cols = 9;
+const matrix = [];
+for(let i = 0; i < rows; i++){
+  const rowArray = [];
+  for(let j = 0; j < cols; j++){
+    rowArray.push(`Row ${i + 1}, Col ${j + 1}`);
+  }
+  matrix.push(rowArray);
+}
+let part10Text = "";
+for(let i = 0; i < rows; i++){
+  for(let j = 0; j < cols; j++){
+    part10Text += matrix[i][j] + " | ";
+  }
+  part10Text += newLine;
+}
+printOut(part10Text);
 printOut(newLine);
